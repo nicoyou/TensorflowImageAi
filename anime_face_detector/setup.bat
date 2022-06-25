31f156985b3037ff9ff933f909e48b86ec7ba215
羽咲蒼 <50352295+nicoyou@users.noreply.github.com>
2022-06-25 21:28:06 +0900

    clean: 環境構築用ファイルを整理

--- anime_face_detector/setup.bat
+++
@@ -1,6 +0,0 @@
-python -m venv .venv
-call .venv/Scripts/activate.bat
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-
-pause
--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 python -m venv .venv
 call .venv/Scripts/activate.bat
 python -m pip install --upgrade pip
-pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 pause
