3c7fdc9524aa9d6992ad9265e024a5a7793d1eea
羽咲蒼 <50352295+nicoyou@users.noreply.github.com>
2022-10-06 19:10:15 +0900

    clean: yapf を使用して全プログラムをフォーマット

--- image_ai/tf_callback.py
+++ image_ai/tf_callback.py
@@ -2,13 +2,15 @@ import tensorflow as tf
 
 
 class TimeCallback(tf.keras.callbacks.Callback):
-	def __init__(self, metric_name = "time"):
-		self.metric_name = metric_name
-		self.times = []
-		return
-	def on_epoch_begin(self, epoch, logs=None):
-		self.timetaken = tf.timestamp()
-		return
-	def on_epoch_end(self, epoch, logs = {}):
-		logs[self.metric_name] = float(tf.timestamp() - self.timetaken)
-		return
+    def __init__(self, metric_name="time"):
+        self.metric_name = metric_name
+        self.times = []
+        return
+
+    def on_epoch_begin(self, epoch, logs=None):
+        self.timetaken = tf.timestamp()
+        return
+
+    def on_epoch_end(self, epoch, logs={}):
+        logs[self.metric_name] = float(tf.timestamp() - self.timetaken)
+        return
